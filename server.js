6972d9b34736b89c1da8e671b7d6d806561559c6
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-09-17 22:48:13 -0500

    Deploying

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ mongoose.connect(MONGODB_URI, {
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./Develop/routes/api.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
